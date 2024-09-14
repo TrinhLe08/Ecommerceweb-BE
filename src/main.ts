7216251e81c5f102f758855ae13476e072355020
TrinhLe08 <khanhtrinhmc2021@gmail.com>
2024-09-14 08:12:04 +0700

    done

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.URL_FRONTEND],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
